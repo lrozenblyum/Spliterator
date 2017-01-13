a4236ba6b77399f905d0c706c410b81e58e0f8b8
Leonid Rozenblyum <lrozenblyum@gmail.com>
2017-01-13 21:33:38 +0200

    Dummy implementation

--- spliterator/src/main/java/com/leokom/spliterator/ArraySpliterator.java
+++ spliterator/src/main/java/com/leokom/spliterator/ArraySpliterator.java
@@ -1,8 +1,31 @@
 package com.leokom.spliterator;
 
-public class ArraySpliterator<T> {
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class ArraySpliterator<T> implements Spliterator<T> {
 	// trying a generic array while it can cause
 	//covariance problems
 	public ArraySpliterator(T[] array) {
+	}
+
+	@Override
+	public boolean tryAdvance(Consumer<? super T> action) {
+		return false;
+	}
+
+	@Override
+	public Spliterator<T> trySplit() {
+		return null;
+	}
+
+	@Override
+	public long estimateSize() {
+		return 0;
+	}
+
+	@Override
+	public int characteristics() {
+		return 0;
 	}
 }
