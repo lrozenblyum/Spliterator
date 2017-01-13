b09ed4fcfdedddc2dc12d8dc9c44f50561cb4fb4
Leonid Rozenblyum <lrozenblyum@gmail.com>
2017-01-13 21:37:26 +0200

    Check that the stream doesn't sum anything

--- spliterator/src/test/java/com/leokom/spliterator/ArraySpliteratorTest.java
+++ spliterator/src/test/java/com/leokom/spliterator/ArraySpliteratorTest.java
@@ -3,6 +3,8 @@ package com.leokom.spliterator;
 import static org.junit.Assert.*;
 
 import java.util.Spliterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.junit.Test;
 
@@ -12,5 +14,16 @@ public class ArraySpliteratorTest {
 		String [] array = {};
 		
 		assertTrue( new ArraySpliterator<String>( array ) instanceof Spliterator );
+	}
+	
+	@Test
+	public void sequentialStreamOfSpliterator() {
+		Integer [] array = { 1, 2, 3 };
+		
+		ArraySpliterator<Integer> spliterator = new ArraySpliterator<Integer>( array );
+		
+		Stream<Integer> stream = StreamSupport.stream( spliterator, false );
+		
+		assertEquals( 6, stream.mapToInt(Integer::intValue ).sum() );
 	}
 }
